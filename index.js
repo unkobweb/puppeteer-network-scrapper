455dd52b25db1abd85babf2b90123a835893e300
unkobweb <sieg.alexandre@gmail.com>
2024-12-24 15:57:11 +0100

    dans le cul le devtool chrome

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-const puppeteer = require('puppeteer');
-const { consola } = require('consola');
+import puppeteer from 'puppeteer';
+import { consola } from 'consola';
+import chalk from 'chalk';
 
 (async () => {
   const searchedKeyword = await consola.prompt('Enter the searched keyword: ');
+  const caseSensitive = await consola.prompt('Is the search case sensitive? (yes/no): ', {
+    type: 'confirm'
+  });
 
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
@@ -16,11 +20,32 @@ const { consola } = require('consola');
   // Listen for network responses
   page.on('response', async response => {
     try {
-      const responseBody = await response.text();
-      const searchKeyword = searchedKeyword; // Replace with your search term
+      let responseBody = await response.text();
+      let searchKeyword = searchedKeyword; // Replace with your search term
+
+      if (!caseSensitive) {
+        searchKeyword = searchKeyword.toLowerCase();
+        responseBody = responseBody.toLowerCase();
+      }
 
       if (responseBody.includes(searchKeyword)) {
-        console.log(`Found keyword in response of ${response.url()}`);
+        console.log(`${chalk.greenBright.bold('[KEYWORD FOUND]')} ${response.url()}`);
+        //if repsonse is valid JSON search in all property (recursively) for the keyword and log the property path
+        try {
+          const json = JSON.parse(responseBody);
+          const searchInObject = (obj, searchKeyword, path = '') => {
+            for (const key in obj) {
+              if (typeof obj[key] === 'object') {
+                searchInObject(obj[key], searchKeyword, `${path}.${key}`);
+              } else if (obj[key].includes && obj[key].includes(searchKeyword)) {
+                console.log(`${chalk.yellow.bold('[PATH FOUND]')} ${path}.${key}`);
+              }
+            }
+          };
+          searchInObject(json, searchKeyword);
+        } catch (e) {
+          consola.error(`parsing response body as JSON: ${e}`);
+        }
       }
     } catch (e) {
       // console.error(`Error reading response body: ${e}`);
